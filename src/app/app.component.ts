f514d2de5d5f64c00cef944f4976551e1bf8e145
zeroww <dev.zeroww@gmail.com>
2019-07-13 21:04:05 +0200

    build: updated project for hosting it to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { TabsPage } from '../pages/tabs/tabs';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html',
@@ -10,11 +9,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage: string = 'TabsPage';
 
-  constructor(
-    platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen
-  ) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
--- src/pages/crypto-currency-detail/crypto-currency-detail.module.ts
+++ src/pages/crypto-currency-detail/crypto-currency-detail.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+
 import { CryptoCurrencyDetailPage } from './crypto-currency-detail';
 
 @NgModule({
-  declarations: [CryptoCurrencyDetailPage],
+  declarations: [],
   imports: [IonicPageModule.forChild(CryptoCurrencyDetailPage)],
   entryComponents: [CryptoCurrencyDetailPage],
 })
